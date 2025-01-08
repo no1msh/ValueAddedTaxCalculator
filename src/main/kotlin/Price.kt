73f4e74debed61e7eb91512efa62e827b740ce7a
Bandal <no1msh1217@gmail.com>
2025-01-08 16:13:03 +0900

    feat: 가격은 10억을 초과할 수 없다.

--- src/main/kotlin/Price.kt
+++ src/main/kotlin/Price.kt
@@ -5,8 +5,12 @@ value class Price private constructor(val value: Int) {
 
     companion object {
         internal const val MINIMUM_VALUE = 0
+        internal const val MAXIMUM_VALUE = 1_000_000_000
+
         fun of(newValue: Int): Price {
-            require(newValue >= MINIMUM_VALUE) { "$MINIMUM_VALUE 이상 값이어야 합니다." }
+            require(newValue in MINIMUM_VALUE..MAXIMUM_VALUE) {
+                "$MINIMUM_VALUE 이상 $MAXIMUM_VALUE 이하의 값이어야 합니다."
+            }
             return Price(newValue)
         }
     }
--- src/test/kotlin/PriceTest.kt
+++ src/test/kotlin/PriceTest.kt
@@ -1,6 +1,9 @@
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.matchers.shouldBe
 import org.example.Price
+import org.example.Price.Companion.MAXIMUM_VALUE
 import org.example.Price.Companion.MINIMUM_VALUE
 import org.junit.jupiter.api.assertThrows
 
@@ -11,6 +14,23 @@ class PriceTest : AnnotationSpec() {
 
         val error = assertThrows<IllegalArgumentException> { Price.of(newValue) }
 
-        error.message shouldBe "$MINIMUM_VALUE 이상 값이어야 합니다."
+        error.message shouldBe "$MINIMUM_VALUE 이상 $MAXIMUM_VALUE 이하의 값이어야 합니다."
+    }
+
+    @Test
+    fun `금액이 10억을 초과한다면 런타임 에러가 발생한다`() {
+        val newValue = 1_000_000_001
+
+        val error = assertThrows<IllegalArgumentException> { Price.of(newValue) }
+
+        error.message shouldBe "$MINIMUM_VALUE 이상 $MAXIMUM_VALUE 이하의 값이어야 합니다."
+    }
+
+    @Test
+    fun `금액이 0이상 10억이하면 금액 객체를 생성할 수 있다`() {
+        assertSoftly {
+            shouldNotThrowAny { Price.of(0) }
+            shouldNotThrowAny { Price.of(1_000_000_000) }
+        }
     }
 }
